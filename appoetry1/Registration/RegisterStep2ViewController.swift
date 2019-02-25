03c3b525b22338cf8196315e4d5f8c4c545658c9
Kristaps Bremers <kristaps.bremers@chi.lv>
2019-02-25 16:21:21 +0200

    Made a selection for date of birth textfield

--- appoetry1/Registration/RegisterStep2ViewController.swift
+++ appoetry1/Registration/RegisterStep2ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class RegisterStep2ViewController: UIViewController {
-    var showMeAgain: (() -> Void)?
+    @IBOutlet weak var fullName: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var dateOfBirth: UITextField!
+    
+    
+    let datePicker = UIDatePicker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        showDatePicker()
+    }
+    
+    
+    func showDatePicker(){
+        //Formate Date
+        datePicker.datePickerMode = .date
         
-        // Do any additional setup after loading the view.
+        //ToolBar
+        let toolbar = UIToolbar();
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(donedatePicker));
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelDatePicker));
+        
+        toolbar.setItems([doneButton,spaceButton,cancelButton], animated: false)
+        
+        dateOfBirth?.inputAccessoryView = toolbar
+        dateOfBirth?.inputView = datePicker
+        
+    }
+    
+    @objc func donedatePicker(){
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        dateOfBirth.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    @objc func cancelDatePicker(){
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func pressFromStepTwo(_ sender: Any) {
     }
     
     @IBAction func backToStepOne(_ sender: Any) {
