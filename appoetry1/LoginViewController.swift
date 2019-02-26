d1d71d8d44ed6bceb8def9a7e9132df068386074
Kristaps Bremers <kristaps.bremers@chi.lv>
2019-02-26 17:36:55 +0200

    Email Login verification alert works. corrected some other changes

--- appoetry1/LoginViewController.swift
+++ appoetry1/LoginViewController.swift
@@ -11,43 +11,62 @@ import UIKit
 class LoginViewController: UIViewController {
     @IBOutlet weak var emailLogin: UITextField!
     @IBOutlet weak var passwordLogin: UITextField!
-    @IBOutlet weak var validationMessage: UILabel!
-        
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        validationMessage.isHidden = true
     }
     
     @IBAction func pressLogin(_ sender: Any) {
+
+        let providedEmailAddress = emailLogin.text
         
-        validationMessage.isHidden = true
+        let isEmailAddressValid = isValidEmailAddress(emailAddressString: providedEmailAddress!)
         
-        guard let email = emailLogin.text, emailLogin.text?.characters.count != 0 else {
-            validationMessage.isHidden = false
-            validationMessage.text = "Please enter your email!"
-            return
+        if isEmailAddressValid {
+            
+            print("Email address is valid")
+        } else {
+            print("Email address is not valid")
+            displayAlertMessage(messageToDisplay: "Email address is not valid")
         }
-        
-        if isValidEmail(emailID:email) == false {
-            validationMessage.isHidden = false
-            validationMessage.text = "Please enter valid email address!"
-        }
-        
-        guard let password = passwordLogin.text, passwordLogin.text?.characters.count != 0 else {
-            validationMessage.isHidden = false
-            validationMessage.text = "Please enter your password"
-            return
-        }
-        
-        NotificationCenter.default.post(name: Notification.Name("goToMain"), object: nil)
     }
     
-    func isValidEmail(emailID:String) -> Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
-        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        return emailTest.evaluate(with: emailID)
+    func isValidEmailAddress(emailAddressString: String) -> Bool {
+        
+        var returnValue = true
+        let emailRegEx = "[A-Z0-9a-z.-_]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,3}"
+        
+        do {
+            let regex = try NSRegularExpression(pattern: emailRegEx)
+            let nsString = emailAddressString as NSString
+            let results = regex.matches(in: emailAddressString, range: NSRange(location: 0, length: nsString.length))
+            
+            if results.count == 0
+            {
+                returnValue = false
+            }
+            
+        } catch let error as NSError {
+            print("invalid regex: \(error.localizedDescription)")
+            returnValue = false
+        }
+        
+        return  returnValue
     }
-    
+    func displayAlertMessage(messageToDisplay: String)
+    {
+        let alertController = UIAlertController(title: "Alert", message: messageToDisplay, preferredStyle: .alert)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
+            
+            print("Ok button tapped");
+            }
+        
+        alertController.addAction(OKAction)
+        
+        self.present(alertController, animated: true, completion:nil)
+    }
+
     @IBAction func pressRegister(_ sender: Any) {
         NotificationCenter.default.post(name: Notification.Name("goToReg"), object: nil)
     }
