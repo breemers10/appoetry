4c6e72ce41f22f5558a7bf0e90b47401608d613e
Kristaps Bremers <kristaps.bremers@chi.lv>
2019-03-07 17:53:58 +0200

    Made a new view controller file for creating posts and added to storyboard

---
+++ appoetry1/Create a Post/CreatePostViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CreatePostViewController.swift
+//  appoetry1
+//
+//  Created by Kristaps Brēmers on 07.03.19.
+//  Copyright © 2019. g. Chili. All rights reserved.
+//
+
+import UIKit
+
+class CreatePostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
+extension CreatePostViewController: ClassName {
+    static var className: String {
+        return String(describing: self)
+    }
+}
