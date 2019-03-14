5468c0e1016e6197187059ff2a16d391006ad2d2
Kristaps Bremers <kristaps.bremers@chi.lv>
2019-03-14 15:15:44 +0200

    created a CreatePostViewModel

---
+++ appoetry1/Create a Post/CreatePostViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CreatePostViewModel.swift
+//  appoetry1
+//
+//  Created by Kristaps Brēmers on 14.03.19.
+//  Copyright © 2019. g. Chili. All rights reserved.
+//
+
+import UIKit
+
+class CreatePostViewModel: NSObject {
+
+}
