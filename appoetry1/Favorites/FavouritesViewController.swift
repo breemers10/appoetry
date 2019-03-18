7a37f68c447aba2bd69bcc775c191957a8123c29
Kristaps Bremers <kristaps.bremers@chi.lv>
2019-03-18 17:36:53 +0200

    favourite posts feed also works fine

--- appoetry1/Favorites/FavouritesViewController.swift
+++ appoetry1/Favorites/FavouritesViewController.swift
@@ -7,17 +7,70 @@
 //
 
 import UIKit
+import Firebase
 
-class FavouritesViewController: UIViewController {
+class FavouritesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     var viewModel: FavouritesViewModel?
     let createPostButton = UIButton(type: .system)
     @IBOutlet weak var collectionView: UICollectionView!
     
+    var posts = [Post]()
+    var favouritedPosts = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBarItems()
         addingTargetToCreatePostVC()
+        fetchPosts()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    func fetchPosts() {
+        AppDelegate.instance().showActivityIndicator()
+        
+        let uid = Auth.auth().currentUser?.uid
+        
+        MySharedInstance.instance.ref.child("users").child(uid!).child("favouritedPosts").queryOrderedByKey().observeSingleEvent(of: .value, with: { snapshot in
+            _ = snapshot.value as! [String : AnyObject]
+            
+            self.favouritedPosts.append(Auth.auth().currentUser!.uid)
+            AppDelegate.instance().dismissActivityIndicator()
+        })
+        
+        MySharedInstance.instance.ref.child("posts").observeSingleEvent(of: .value, with: { (snap) in
+            let postSnap = snap.value as! [String: AnyObject]
+            
+            for (_,post) in postSnap {
+                if let userID = post["userID"] as? String {
+                    for each in self.favouritedPosts {
+                        if each == userID {
+                            let posst = Post()
+                            if let author = post["author"] as? String, let favourites = post["favourites"] as? Int, let pathToImage = post["pathToImage"] as? String, let postID = post["postID"] as? String, let poem = post["poem"] as? String {
+                                posst.username = author
+                                posst.favourites = favourites
+                                posst.pathToImage = pathToImage
+                                posst.postID = postID
+                                posst.userID = userID
+                                posst.poem = poem
+                                
+                                if let people = post["peopleFavourited"] as? [String : AnyObject] {
+                                    for (_,person) in people {
+                                        posst.peopleFavourited.append(person as! String)
+                                    }
+                                }
+                                self.posts.append(posst)
+                            }
+                        }
+                    }
+                    AppDelegate.instance().dismissActivityIndicator()
+                    self.collectionView.reloadData()
+                }
+            }
+        })
+        MySharedInstance.instance.ref.removeAllObservers()
     }
     
     @objc func createPostButtonPressed(sender: UIButton) {
@@ -39,6 +92,33 @@ class FavouritesViewController: UIViewController {
         
         navigationController?.navigationBar.titleTextAttributes = titleTextAttributed
         navigationItem.title = "Appoetry"
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.posts.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "favouritePostCell", for: indexPath) as! FavouriteFeedViewCell
+        cell.postImage.downloadImage(from: self.posts[indexPath.row].pathToImage)
+        cell.authorLabel.text = self.posts[indexPath.row].username
+        cell.textView.text = self.posts[indexPath.row].poem
+        cell.textView.isEditable = false
+        cell.favouritesLabel.text = "\(self.posts[indexPath.row].favourites!) Favourites"
+        cell.postID = self.posts[indexPath.row].postID
+        
+        for person in self.posts[indexPath.row].peopleFavourited {
+            if person == Auth.auth().currentUser!.uid {
+                cell.favouriteButton.isHidden = true
+                cell.unfavouriteButton.isHidden = false
+                break
+            }
+        }
+        return cell
     }
 }
 
