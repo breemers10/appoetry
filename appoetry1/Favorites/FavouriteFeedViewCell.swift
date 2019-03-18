8b6facfbb39c54734d6284527afb3b2f0173c4b7
Kristaps Bremers <kristaps.bremers@chi.lv>
2019-03-18 17:36:53 +0200

    favourite posts feed also works fine

--- appoetry1/Favorites/FavouriteFeedViewCell.swift
+++ appoetry1/Favorites/FavouriteFeedViewCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class FavouriteFeedViewCell: UICollectionViewCell {
     @IBOutlet weak var postImage: UIImageView!
@@ -15,6 +16,75 @@ class FavouriteFeedViewCell: UICollectionViewCell {
     @IBOutlet weak var favouritesLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var favouriteButton: UIButton!
-    
     @IBOutlet weak var unfavouriteButton: UIButton!
+    
+    var postID: String!
+    
+    @IBAction func favouriteButtonPressed(_ sender: Any) {
+        self.favouriteButton.isHidden = false
+        let keyToPost = MySharedInstance.instance.ref.child("posts").childByAutoId().key!
+        
+        MySharedInstance.instance.ref.child("posts").child(self.postID).observeSingleEvent(of: .value, with: { (snapshot) in
+            if let _ = snapshot.value as? [String: AnyObject] {
+                let updateFavourites: [String : Any] = [ "peopleFavourited/\(keyToPost)" : Auth.auth().currentUser!.uid]
+                MySharedInstance.instance.ref.child("posts").child(self.postID).updateChildValues(updateFavourites, withCompletionBlock: { (error, ref) in
+                    if error == nil {
+                        MySharedInstance.instance.ref.child("posts").child(self.postID).observeSingleEvent(of: .value, with: { (snap) in
+                            if let properties = snap.value as? [String : AnyObject] {
+                                if let favourites = properties["peopleFavourited"] as? [String : AnyObject] {
+                                    let count = favourites.count
+                                    self.favouritesLabel.text = "\(count) Favourites"
+                                    
+                                    let update = ["favourites" : count]
+                                    MySharedInstance.instance.ref.child("posts").child(self.postID).updateChildValues(update)
+                                    
+                                    self.favouriteButton.isHidden = true
+                                    self.unfavouriteButton.isHidden = false
+                                    self.favouriteButton.isEnabled = true
+                                }
+                            }
+                        })
+                    }
+                })
+            }
+        })
+        MySharedInstance.instance.ref.removeAllObservers()
+    }
+    
+    @IBAction func unfavouriteButtonPressed(_ sender: Any) {
+        self.unfavouriteButton.isEnabled = false
+        MySharedInstance.instance.ref.child("posts").child(self.postID).observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            if let properties = snapshot.value as? [String : AnyObject] {
+                if let peopleFavourited = properties["peopleFavourited"] as? [String : AnyObject] {
+                    
+                    
+                    for (id, person) in peopleFavourited {
+                        if person as? String == Auth.auth().currentUser!.uid {
+                            MySharedInstance.instance.ref.child("posts").child(self.postID).child("peopleFavourited").child(id).removeValue(completionBlock: { (error, ref) in
+                                if error == nil {
+                                    MySharedInstance.instance.ref.child("posts").child(self.postID).observeSingleEvent(of: .value, with: { (snap) in
+                                        if let prop = snap.value as? [String : AnyObject] {
+                                            if let favourites = prop["peopleFavourited"] as? [String : AnyObject] {
+                                                let count = favourites.count
+                                                self.favouritesLabel.text = "\(count) Favourites"
+                                                MySharedInstance.instance.ref.child("posts").child(self.postID).updateChildValues(["favourites" : count]) } else {
+                                                self.favouritesLabel.text = "0 Favourites"
+                                                MySharedInstance.instance.ref.child("posts").child(self.postID).updateChildValues(["favourites" : 0])
+                                            }
+                                        }
+                                    })
+                                }
+                            })
+                            
+                            self.favouriteButton.isHidden = false
+                            self.unfavouriteButton.isHidden = true
+                            self.unfavouriteButton.isEnabled = true
+                            break
+                        }
+                    }
+                }
+            }
+        })
+    }
 }
