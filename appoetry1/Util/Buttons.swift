f2a2c24e23260420a394d917cbb18f36089ddd16
Kristaps Bremers <kristaps.bremers@chi.lv>
2019-03-21 17:07:55 +0200

    changed styles for buttons using a customview

---
+++ appoetry1/Util/Buttons.swift
@@ -0,0 +1,36 @@
+//
+//  loginButton.swift
+//  appoetry1
+//
+//  Created by Kristaps Brēmers on 21.03.19.
+//  Copyright © 2019. g. Chili. All rights reserved.
+//
+
+import UIKit
+
+class Buttons: UIButton {
+
+    @IBInspectable var rounded: CGFloat = 0 { didSet {
+        self.layer.cornerRadius = rounded
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 { didSet {
+        self.layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+}
