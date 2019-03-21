d923d9a3d770a985d050fd7aca3d0ed4730073cb
Kristaps Bremers <kristaps.bremers@chi.lv>
2019-03-21 10:47:18 +0200

    made a viewcontroller in stroyboard

---
+++ appoetry1/Following/FollowingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FollowingTableViewCell.swift
+//  appoetry1
+//
+//  Created by Kristaps Brēmers on 21.03.19.
+//  Copyright © 2019. g. Chili. All rights reserved.
+//
+
+import UIKit
+
+class FollowingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
