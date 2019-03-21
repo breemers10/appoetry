9169997288753fcca5826ee23b7552350784a8c4
Kristaps Bremers <kristaps.bremers@chi.lv>
2019-03-21 10:43:19 +0200

    made a view controller and viewmodel for follower screen

---
+++ appoetry1/Following/FollowingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FollowingViewController.swift
+//  appoetry1
+//
+//  Created by Kristaps Brēmers on 21.03.19.
+//  Copyright © 2019. g. Chili. All rights reserved.
+//
+
+import UIKit
+
+class FollowingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ appoetry1/Following/FollowingViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FollowingViewModel.swift
+//  appoetry1
+//
+//  Created by Kristaps Brēmers on 21.03.19.
+//  Copyright © 2019. g. Chili. All rights reserved.
+//
+
+import UIKit
+
+class FollowingViewModel: NSObject {
+
+}
